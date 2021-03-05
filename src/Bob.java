a01723a85632dfff30c5273086654c20dc453f5c
Carlos Padilla <mr.carlos.padilla@gmail.com>
2021-03-05 14:10:38 -0600

    feat: completed with 2nd part of strings exercise

---
+++ src/Bob.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Bob {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hi, I'm Bob.\n");
+        System.out.println("You can: \nAsk Bob a question\nYell at Bob\nSay nothing to Bob\nSay whatever you want to Bob\n");
+        System.out.println("Type below:");
+        String userInput = sc.nextLine();
+
+        if(userInput.endsWith("?")) {
+            System.out.println("\nBob: Sure.");
+        } else if(userInput.endsWith("!")) {
+            System.out.println("\nBob: Whoa, chill out!");
+        } else if(userInput.endsWith(" ")) {
+            System.out.println("\nBob: Fine. Be that way!");
+        } else {
+            System.out.println("\nBob: Whatever.");
+        }
+    }
+}
