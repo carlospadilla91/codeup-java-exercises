c4f3d5ab7d71cb4bd28eae144c248fd4b321bc05
Carlos Padilla <mr.carlos.padilla@gmail.com>
2021-03-10 22:11:59 -0600

    feat: working on oop exercise

--- src/util/Input.java
+++ src/util/Input.java
@@ -4,5 +4,33 @@ import java.util.Scanner;
 
 public class Input {
 
-    private Scanner scanner;
+    private final Scanner scanner;
+
+    //TODO: When an instance of this object is created, the scanner property should be set to a new instance of the Scanner class.
+
+    public Input(){
+        this.scanner = new Scanner(System.in);
+    }
+
+    public String getString(){
+        return this.scanner.next();
+    }
+
+    public boolean yesNo(){
+        System.out.println("Please enter yes/no: ");
+        String userInput = this.scanner.next();
+        return userInput.equalsIgnoreCase("yes") || userInput.equalsIgnoreCase("y");
+    }
+//
+//    public int getInt(int min, int max){
+//        System.out.println("Please enter number between " + min + " and " + max);
+//        int userNum = this.scanner.nextInt();
+//        if (userNum >= min && userNum <= max){
+//            return userNum;
+//        }
+//    }
+
+
 }
+
+
