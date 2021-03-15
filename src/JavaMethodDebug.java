7b3f35c2846f6e3c0d9b61fb25b481406b3d48e6
Carlos Padilla <mr.carlos.padilla@gmail.com>
2021-03-15 10:01:16 -0500

    feat: debug exercise

---
+++ src/JavaMethodDebug.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+
+public class JavaMethodDebug {
+
+    // TODO: Fix the method below to work as expected.
+    //  The method should take in a string and character and return the number of instances of the passed character in the string.
+    public static int countLetters(String input, char character) {
+        int charCount = 0;
+        for (int i = 0; i < input.length(); i += 1) {
+            if (Character.toLowerCase(input.charAt(i)) == Character.toLowerCase(character)){
+                charCount++;
+            }
+//            if (input.equalsIgnoreCase(String.valueOf(Character.toLowerCase(character)))) {
+//                charCount++;
+//            }
+        }
+        return charCount;
+    }
+
+    public static void main(String[] args) {
+        int numberOfSs = countLetters("MissiSSippi", 's');
+        System.out.println(numberOfSs); // should return 4
+        int numberOfPs = countLetters("Mississippi", 'p');
+        System.out.println(numberOfPs);
+    }
+}
