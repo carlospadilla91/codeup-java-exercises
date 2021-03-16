0ada05dabaeb260028fb2a91c4222122e751f699
Carlos Padilla <mr.carlos.padilla@gmail.com>
2021-03-15 21:06:36 -0500

    feat: completing mini exercises

--- src/inheritance/Ingredient.java
+++ src/inheritance/Ingredient.java
@@ -1,6 +1,5 @@
 package inheritance;
 
-import org.w3c.dom.ls.LSOutput;
 
 public class Ingredient {
 
--- src/inheritance/IngredientTest.java
+++ src/inheritance/IngredientTest.java
@@ -1,11 +1,24 @@
 package inheritance;
 
+import jelly.Jelly;
+
 public class IngredientTest {
 
+    public static void throwAwayIngredient(Ingredient i){
+        i.throwAway();
+    }
 
 
 
     public static void main(String[] args) {
+
+        Ingredient j = new Jelly();
+        Ingredient pb = new PeanutButter();
+        Ingredient b = new Bread();
+
+        throwAwayIngredient(j);
+        throwAwayIngredient(pb);
+        throwAwayIngredient(b);
 
 
     }
